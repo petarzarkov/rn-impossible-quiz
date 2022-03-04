f8f0ea7b29d4872471a021ccef118f1d4b401996
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2022-03-04 21:47:30 +0200

    add proper html decoding

--- app/utils/decodeHtml.ts
+++ app/utils/decodeHtml.ts
@@ -1,23 +1,16 @@
+import { htmlSymbols } from "./htmlSymbols";
+
 export const decodeHtml = (text: string) => {
   if (!text) {
     return;
   }
   try {
-    text = text.trim();
-    text = text.replace(/%/g, "");
-    // eslint-disable-next-line prettier/prettier
-    text = text.replace(/&quot;/g, "\"");
-    text = text.replace(/&#039;/g, "'");
-    text = text.replace(/&aacute;/gi, "á");
-    text = text.replace(/&ntilde;/gi, "ñ");
-    text = text.replace(/&pi;/gi, "π");
-    text = text.replace(/&ldquo;/gi, "“");
-    text = text.replace(/&rdquo;/gi, "”");
-    text = text.replace(/&uacute;/gi, "ú");
-    text = text.replace(/&atilde;/gi, "ã");
-    text = text.replace(/&oacute;/gi, "ó");
-    text = text.replace(/&uuml/gi, "ü");
-    return text;
+    return text.replace(/&(.+);/gi, (match: string) => {
+      if (htmlSymbols[match]) {
+        return htmlSymbols[match];
+      }
+      return match;
+    });
   } catch (error) {
     return text;
   }
